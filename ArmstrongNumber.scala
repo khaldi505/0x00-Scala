f6be3a9f883a3dcf59f12ca5a3368b5d3e45d689
khaldi505 <ihebkhaldi505@gmail.com>
2023-06-30 08:37:06 +0100

    last task

--- ArmstrongNumber.scala
+++ ArmstrongNumber.scala
@@ -1,22 +1,20 @@
 import scala.io.StdIn.readInt
-
 object ArmstrongNumber extends App {
-    
-    def armStrongNumber(n: Int) = {
-        var temp = n
-        var p = 0
-        while (n >0)
-        rem = n % 10
-        p = (p) + (rem * rem * rem)
-        n = n / 10
-        if (temp == p)
-            print("armstrong")
-        else
-            print("not a armstrong number")
-    }
+  def isArmstrongNumber(number: Int): Boolean = {
+    val digits = number.toString.map(_.asDigit)
+    val sumOfPowers = digits.map(x => x * x * x).sum
+    number == sumOfPowers
+  }
 
-    print("please enter a number")
-    var number = readInt()
-    x = number.toString()
-    print(x[0])
+  print("Enter a three-digit number: ")
+  var number = readInt()
+
+  val isArmstrong = isArmstrongNumber(number)
+
+  if (isArmstrong) {
+    println("The number is an Armstrong number.")
+  } else {
+    println("The number is not an Armstrong number.")
+  }
+
 }
